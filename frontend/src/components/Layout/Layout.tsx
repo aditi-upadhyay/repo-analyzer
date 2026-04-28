63c23c49f8fcfba224e4a2ab20bcdf9aaf207c01
Aditi <aditibupadhyay@gmail.com>
2026-04-28 22:13:39 +0530

    refactor: update layouy component to make it responsive

--- frontend/src/components/Layout/Layout.tsx
+++ frontend/src/components/Layout/Layout.tsx
@@ -2,17 +2,34 @@ import SideBar from "./SideBar";
 import NavBar from "./NavBar";
 import { Outlet } from "react-router-dom";
 import { DocumentationProvider } from "../../context/DocumentationContext";
+import { useState } from "react";
 
 function Layout() {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
     return (
         <DocumentationProvider>
-            <div className="flex h-screen">
-                <div className="w-1/6">
-                    <SideBar />
+            <div className="flex h-screen bg-gray-50 overflow-hidden relative">
+                {/* Mobile sidebar overlay */}
+                {isSidebarOpen && (
+                    <div 
+                        className="fixed inset-0 bg-black bg-opacity-50 z-40 md:hidden"
+                        onClick={() => setIsSidebarOpen(false)}
+                    />
+                )}
+
+                {/* Sidebar */}
+                <div className={`
+                    fixed inset-y-0 left-0 z-50 w-64 transform transition-transform duration-300 ease-in-out
+                    md:relative md:translate-x-0 md:w-1/4 lg:w-1/6 h-full
+                    ${isSidebarOpen ? "translate-x-0" : "-translate-x-full"}
+                `}>
+                    <SideBar onClose={() => setIsSidebarOpen(false)} />
                 </div>
-                <div className="flex-1 flex flex-col h-screen overflow-hidden">
-                    <div className="flex-shrink-0">
-                        <NavBar title="Repo Analyzer" />
+
+                <div className="flex-1 flex flex-col h-screen overflow-hidden min-w-0">
+                    <div className="flex-shrink-0 z-10 relative">
+                        <NavBar title="Repo Analyzer" onMenuClick={() => setIsSidebarOpen(true)} />
                     </div>
                     <div className="flex-1 overflow-y-auto bg-gray-50">
                         <Outlet />
